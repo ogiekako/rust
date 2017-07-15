17652d4a90cd5b11bcf0b2f38731ed03dccfb8a4
Keigo Oka <ogiekako@gmail.com>
2017-07-15 23:07:23 +0900

    ARC 078

---
+++ solutions/atcoder/arc078/src/d.rs
@@ -0,0 +1,92 @@
+use std::io;
+use std::io::BufRead;
+
+fn main() {
+    let mut sc = Scanner::new(std::io::stdin());
+    let n: usize = sc.next().unwrap();
+
+    let mut g: Vec<Vec<usize>> = vec![vec![]; n];
+    for _ in 0..n - 1 {
+        let a: usize = sc.next().unwrap();
+        let b: usize = sc.next().unwrap();
+        let a = a - 1;
+        let b = b - 1;
+        g[a].push(b);
+        g[b].push(a);
+    }
+
+    let mut d1 = vec![0; n];
+    let mut d2 = vec![0; n];
+
+    f(&g, &mut d1, 0, n, 0);
+    f(&g, &mut d2, n - 1, n, 0);
+
+    let mut a = 0;
+    for i in 0..n {
+        if d1[i] <= d2[i] {
+            a += 1;
+        }
+    }
+    println!("{}", if a > n - a { "Fennec" } else { "Snuke" });
+}
+
+fn f(g: &Vec<Vec<usize>>, ds: &mut Vec<usize>, i: usize, p: usize, d: usize) {
+    ds[i] = d;
+    for j in g[i].iter() {
+        if *j != p {
+            f(g, ds, *j, i, d + 1);
+        }
+    }
+}
+
+/// Scanner
+///
+/// # Example
+/// ```
+/// use contest::io::scanner;
+/// let mut sc = scanner::Scanner::new("1 2 \n\n \r\t \n 3.5".as_bytes());
+/// assert_eq!("1".to_string(), sc.next::<String>().unwrap());
+/// assert_eq!(2, sc.next().unwrap());
+/// assert_eq!(3.5, sc.next().unwrap());
+/// assert_eq!(None, sc.next::<i32>());
+/// ```
+
+pub struct Scanner<R: io::Read> {
+    br: io::BufReader<R>,
+    // Read tokens are stored in reversed order per line.
+    buf: Vec<String>,
+}
+
+impl<R: io::Read> Scanner<R> {
+    pub fn new(r: R) -> Scanner<R> {
+        Scanner {
+            br: io::BufReader::new(r),
+            buf: vec![],
+        }
+    }
+    #[inline]
+    pub fn next<T>(&mut self) -> Option<T>
+        where T: std::str::FromStr,
+              T::Err: std::fmt::Debug
+    {
+        self.next_string().map(|s| s.parse::<T>().expect("Parse failed: "))
+    }
+    fn next_string(&mut self) -> Option<String> {
+        self.buf.pop().or_else(|| match self.update() {
+            true => self.next_string(),
+            false => None,
+        })
+    }
+    #[inline]
+    fn update(&mut self) -> bool {
+        let mut s = String::new();
+        let res = self.br.read_line(&mut s);
+        match res.expect("I/O error.") {
+            0 => false,
+            _ => {
+                self.buf = s.split_whitespace().map(|x| x.to_string()).rev().collect();
+                true
+            }
+        }
+    }
+}
---
+++ solutions/atcoder/arc078/src/e.rs
@@ -0,0 +1,117 @@
+use std::io;
+use std::io::BufRead;
+
+fn answer(n: u64) {
+    println!("! {}", n);
+}
+
+
+fn main() {
+    let mut sc = Scanner::new(std::io::stdin());
+
+    if favorite(&mut sc, 10_000_000_000) {
+        let mut ans = 1;
+        loop {
+            if favorite(&mut sc, ans * 2) {
+                answer(ans);
+                return;
+            }
+            ans *= 10;
+        }
+    }
+
+    let mut dig = 1;
+    let mut ten = 1;
+    loop {
+        if !favorite(&mut sc, ten * 10) {
+            break;
+        }
+        ten *= 10;
+        dig += 1;
+    }
+
+    let mut head = 0;
+    for i in 0..dig {
+        let mut left: i32 = -1;
+        let mut right: i32 = 10;
+        while right - left > 1 {
+            let mid = (left + right) / 2;
+            if favorite(&mut sc,
+                        (head * 10 + mid as u64) * (if i + 1 == dig { 10 } else { 1 })) {
+                if i + 1 < dig {
+                    left = mid;
+                } else {
+                    right = mid;
+                }
+            } else {
+                if i + 1 < dig {
+                    right = mid;
+                } else {
+                    left = mid;
+                }
+            }
+        }
+        head = head * 10 + left as u64;
+    }
+    answer(head + 1);
+}
+
+fn favorite(sc: &mut Scanner<std::io::Stdin>, n: u64) -> bool {
+    if n == 0 {
+        return true;
+    }
+    println!("? {}", n);
+    sc.next::<String>().unwrap() == "Y".to_string()
+}
+
+/// Scanner
+///
+/// # Example
+/// ```
+/// use contest::io::scanner;
+/// let mut sc = scanner::Scanner::new("1 2 \n\n \r\t \n 3.5".as_bytes());
+/// assert_eq!("1".to_string(), sc.next::<String>().unwrap());
+/// assert_eq!(2, sc.next().unwrap());
+/// assert_eq!(3.5, sc.next().unwrap());
+/// assert_eq!(None, sc.next::<i32>());
+/// ```
+
+pub struct Scanner<R: io::Read> {
+    br: io::BufReader<R>,
+    // Read tokens are stored in reversed order per line.
+    buf: Vec<String>,
+}
+
+impl<R: io::Read> Scanner<R> {
+    pub fn new(r: R) -> Scanner<R> {
+        Scanner {
+            br: io::BufReader::new(r),
+            buf: vec![],
+        }
+    }
+    #[inline]
+    pub fn next<T>(&mut self) -> Option<T>
+        where T: std::str::FromStr,
+              T::Err: std::fmt::Debug
+    {
+        self.next_string().map(|s| s.parse::<T>().expect("Parse failed: "))
+    }
+    fn next_string(&mut self) -> Option<String> {
+        self.buf.pop().or_else(|| match self.update() {
+            true => self.next_string(),
+            false => None,
+        })
+    }
+    #[inline]
+    fn update(&mut self) -> bool {
+        let mut s = String::new();
+        let res = self.br.read_line(&mut s);
+        match res.expect("I/O error.") {
+            0 => false,
+            _ => {
+                self.buf = s.split_whitespace().map(|x| x.to_string()).rev().collect();
+                true
+            }
+        }
+    }
+}
